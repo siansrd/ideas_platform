151ff93a61e9079487f2bc3caad854915300c5b5
siansrd <sianrobinsondavies@gmail.com>
2017-08-01 14:31:55 +0100

    Remove un-needed entry model

--- client/src/models/entry.js
+++
@@ -1,8 +0,0 @@
-var Entry = function(entry) {
-  this.title   = entry.title,
-  this.content = entry.content,
-  this.author  = entry.author,
-  this.date    = entry.date
-}
-
-module.exports = Entry
