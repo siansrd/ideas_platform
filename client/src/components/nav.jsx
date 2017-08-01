969baa5256991cece5d2c3fd6528bbc3e55eb023
siansrd <sianrobinsondavies@gmail.com>
2017-08-01 15:06:58 +0100

    Rename UI to Nav

---
+++ client/src/components/nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import About from './about.jsx';
+import List from '../containers/list.jsx';
+import {Link} from 'react-router-dom'
+
+class Nav extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <header>
+          <h1>ideas_platform</h1>
+          <nav id="nav-main">
+            <ul className="nav-list">
+              <li className="nav-item"><Link to='/about'>About</Link></li>
+              <li className="nav-item"><Link to='/list'>List</Link></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+
+}
+
+export default Nav;
+
+
