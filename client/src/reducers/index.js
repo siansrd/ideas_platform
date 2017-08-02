4d257d9007a6804f20771cf7bce3c6c58fba5eae
siansrd <sianrobinsondavies@gmail.com>
2017-08-02 10:44:05 +0100

    Add redux-form to reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import IdeasReducer from './ideas_reducer'
+import { reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
   ideas: IdeasReducer
+  form: formReducer
 });
 
 export default rootReducer;
