02147716006f3b56dc378bc43020b71678fddd30
siansrd <sianrobinsondavies@gmail.com>
2017-08-02 09:25:41 +0100

    Update idea_show component to display idea details

--- client/src/components/ideas_show.jsx
+++ client/src/components/ideas_show.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchIdea } from '../actions'
 
 class IdeasShow extends Component {
 
+  componentWillMount() {
+    console.log("props", this.props)
+    this.props.fetchIdea(this.props.match.params.id)
+  }
+
   render() {
+    const { idea } = this.props
+    if (!idea) return <div>Loading...</div>
     return (
-      <div>Idea</div>
+      <div>
+        <h3>{ idea.title }</h3>
+        <p>{ idea.description }</p>
+        <p>{ idea.created_at }</p>
+        <p>{ idea.user.name }</p>
+      </div>
     )
   }
 
 }
 
-export default IdeasShow
+function mapStateToProps( { ideas }, ownProps ) {
+  return { idea: ideas[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchIdea })(IdeasShow)
